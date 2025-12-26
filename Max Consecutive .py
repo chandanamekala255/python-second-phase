d0a9a9008da8a497a5a89fb5e7510557aba6a192
chandanamekala255 <mekalachandana123@gmail.com>
2025-12-26 15:55:45 +0530

    Create  Max Consecutive .py

---
+++ Max Consecutive .py
@@ -0,0 +1,14 @@
+s=[1,1,1,0,0,1,1,0,1]
+count=0
+res=0
+for num in s:
+    if num==1:
+        count+=1
+    else:
+        if count>res:
+            res=count
+            count=0
+            
+            Max Consecutive 
+print(res)
+#output: 3
