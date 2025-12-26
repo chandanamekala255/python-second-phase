caffff4da592456c64fb58f2320abe3ef26c68e2
chandanamekala255 <mekalachandana123@gmail.com>
2025-12-26 19:51:21 +0530

    Add triangle pattern printing in P7.py
    This code generates a right-aligned triangle pattern of asterisks.

---
+++ P7.py
@@ -0,0 +1,18 @@
+
+n=5
+for i in range (n):
+    for j in range(i):
+        print(" ",end="")
+    for k in range(n-i):
+        print("*",end=" ")
+    print()
+
+
+
+#output:
+#* * * * *
+# * * * *
+#  * * *
+#.  * *
+#.   *
+    
